3cb3c13055032ffb64e03199db8d30d359547072
Jhandie <sumihayjhandie@gmail.com>
2024-10-04 06:31:57 -0700

    dev-object

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello wbayet");
+//Input
+const person1 ={
+    name:"Jhandie Sumihay",
+    age:18,
+    occupation:"pagwapo"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
